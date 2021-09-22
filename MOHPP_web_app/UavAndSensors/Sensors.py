1a6fa181cf39aa1ef087a4d266782ab1c2ce1755
NassimSadallah <nassimmoute@gmail.com>
2021-09-22 15:42:08 +0100

    sensors detection code

--- MOHPP_web_app/UavAndSensors/Sensors.py
+++ MOHPP_web_app/UavAndSensors/Sensors.py
@@ -3,6 +3,30 @@ Created on Jul 8, 2021
 
 @author: nassim
 '''
+import RPi.GPIO as GPIO
+import time
+from threading import Thread
+from multiprocessing import Process
+
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setwarnings(False) # for disable warnings in terminal
+
+# time for sensor to settle
+SENSOR_SETTLE_TIME = 0.00001
+
+MEASURE_INTERVAL_TIME = 0.1 # time delay to measure (min 15miliseconds)                 
+
+# max distance threshold for sensors to react (in cm)
+MAX_DISTANCE_THRESHOLD = 5.0
+
+# Speed of sound at sea level = 343 m/s or 34300 cm/s
+MEASURE_REFERENCE = 17150
+
+# list of sensors
+sensors = []
+
 
 def sensArea():
     
